e17189d4a9b8cbeb0668352f6ebe959c36b43346
小野 成実 <narumi.ono@dxo.co.jp>
2025-06-20 19:19:29 +0900

    wip 修正変更など

--- app/public/js/common.js
+++ app/public/js/common.js
@@ -1,7 +1,7 @@
 //ハンバーガーメニュー開閉動作
-const hamburger = document.getElementById("hamburger-btn");
-const nav = document.getElementById("header-nav");
+const hamburger = document.getElementById('hamburger-btn')
+const nav = document.getElementById('header-nav')
 
-hamburger.addEventListener("click", () => {
-  nav.classList.toggle("open");
-});
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('open')
+})
