38ae9a23b574196e900027810b75b1f4f749844d
小野 成実 <narumi.ono@dxo.co.jp>
2025-06-27 12:23:36 +0900

    distファイル削除

--- app/dist/js/common.js
+++
@@ -1,7 +0,0 @@
-//ハンバーガーメニュー開閉動作
-const hamburger = document.getElementById('hamburger-btn')
-const nav = document.getElementById('header-nav')
-
-hamburger.addEventListener('click', () => {
-  nav.classList.toggle('open')
-})
